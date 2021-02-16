e5cd610945d62ad1d8ca893cbd122e08c04a2026
anwjrrp33 <anwjrrp33@gmail.com>
2021-02-17 03:38:47 +0900

    P296까지 진행

---
+++ board/src/main/java/com/anwjrrp33/board/repository/search/SearchBoardRepository.java
@@ -0,0 +1,7 @@
+package com.anwjrrp33.board.repository.search;
+
+import com.anwjrrp33.board.entity.Board;
+
+public interface SearchBoardRepository {
+    Board search1();
+}
---
+++ board/src/main/java/com/anwjrrp33/board/repository/search/SearchBoardRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.anwjrrp33.board.repository.search;
+
+import com.anwjrrp33.board.entity.Board;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+@Log4j2
+public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
+
+    public SearchBoardRepositoryImpl() {
+        super(Board.class);
+    }
+
+    @Override
+    public Board search1() {
+        log.info("search1..................");
+        return null;
+    }
+}
