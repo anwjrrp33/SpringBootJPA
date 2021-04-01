78d2b1ac8b8c165d509c6525d87da13dec6990d4
anwjrrp33 <anwjrrp33@gmail.com>
2021-04-02 01:07:04 +0900

    P359

---
+++ mreview/src/main/java/com/anwjrrp33/mreview/repository/MovieImageRepository.java
@@ -0,0 +1,7 @@
+package com.anwjrrp33.mreview.repository;
+
+import com.anwjrrp33.mreview.entity.MovieImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieImageRepository extends JpaRepository<MovieImage, Long> {
+}
